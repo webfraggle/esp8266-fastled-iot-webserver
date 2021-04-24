68354f04370dd670e46c1a4766c3f032510988da
Christoph Ketzler <christoph@ketzler.de>
2021-04-24 09:37:09 +0200

    First version of Plasma Animation for LED Matrix

--- esp8266-fastled-iot-webserver.ino
+++ esp8266-fastled-iot-webserver.ino
@@ -60,18 +60,18 @@ extern "C" {
 
 /*######################## MAIN CONFIG ########################*/
 #define LED_TYPE            WS2812B                     // You might also use a WS2811 or any other strip that is Fastled compatible 
-#define DATA_PIN            D3                          // Be aware: the pin mapping might be different on boards like the NodeMCU
+#define DATA_PIN            32                          // Be aware: the pin mapping might be different on boards like the NodeMCU
 //#define CLK_PIN             D5                        // Only required when using 4-pin SPI-based LEDs
 #define CORRECTION          UncorrectedColor            // If colors are weird use TypicalLEDStrip
 #define COLOR_ORDER         GRB                         // Change this if colors are swapped (in my case, red was swapped with green)
-#define MILLI_AMPS          10000                       // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
+#define MILLI_AMPS          8000                       // IMPORTANT: set the max milli-Amps of your power supply (4A = 4000mA)
 #define VOLTS               5                           // Voltage of the Power Supply
 
 #define LED_DEBUG 0                     // enable debug messages on serial console, set to 0 to disable debugging
 
 #define DEFAULT_HOSTNAME "LEDs"         // Name that appears in your network, don't use whitespaces, use "-" instead
 
-#define LED_DEVICE_TYPE 0               // The following types are available
+#define LED_DEVICE_TYPE 1                // The following types are available
 
 /*
     0: Generic LED-Strip: a regular LED-Strip without any special arrangement (and Infinity Mirror + Bottle Lighting Pad)
@@ -565,6 +565,10 @@ PatternAndNameList patterns = {
     { colorWaves_hori,        "Vertical Waves",         true,  true,  false, false, false},
     { colorWaves_vert,        "Color Rings",            true,  true,  false, false, false},
     { rainbow_vert,           "Vertical Rainbow",       true,  true,  false, false, false},
+#endif
+
+#if LED_DEVICE_TYPE == 1                             // palet  speed  color  spark  twinkle
+    { matrixPlasma,            "Matrix Plasma",            false,  true,  false, false, false},
 #endif
 
     // animation patterns                            // palet  speed  color  spark  twinkle
@@ -2364,8 +2368,20 @@ unsigned long patternDelay[ARRAY_SIZE(patterns)] =
 
 void rainbowRoll()
 {
+#if LED_DEVICE_TYPE == 1
+    CRGB tmp_leds[LENGTH];
+    fill_rainbow(tmp_leds, 32, gHue, 7);
+    for (int i = 0; i < LENGTH; i++)
+    {
+        for (int j = 0; j < HEIGHT; j++)
+        {
+          leds[(i*HEIGHT)+j]=tmp_leds[i];
+        }
+    }
+#else
     // FastLED's built-in rainbow generator
     fill_rainbow(leds, NUM_LEDS, gHue, 7);
+#endif
 }
 
 void rainbowBeat()
@@ -3105,6 +3121,47 @@ void DrawDigit(int offset, int segmentLedCount, int r, int g, int b, int n, int 
         dDHelper(offset, 6, s, 0);
     }
 }
+#endif
+
+#if LED_DEVICE_TYPE == 1
+void matrixPlasma()
+{
+    //Serial.print('g');
+    uint16_t ms = millis();
+    for (int i = 0; i < LENGTH; i++) {
+      for (int j = 0; j < HEIGHT; j++) {
+        uint8_t index = inoise8(i * 41, j * 41, ms / 4);  // x and y not change, only change z value with time increment for nice plasma looking noise
+        //leds[XY (i, j)] = CHSV((index + ms / 200) % 255, 255, BRIGHTNESS); // within palette
+        leds[XY (i, j)] = ColorFromPalette (RainbowColors_p, (index + ms / 200) % 255, 255 ); // with palette
+        
+      }
+    }
+    FastLED.delay(1000 / FRAMES_PER_SECOND);
+}
+
+#define LAST_VISIBLE_LED 255
+uint8_t XY (uint8_t x, uint8_t y) {
+  // any out of bounds address maps to the first hidden pixel
+  if ( (x >= LENGTH) || (y >= HEIGHT) ) {
+    return (LAST_VISIBLE_LED + 1);
+  }
+
+  const uint8_t XYTable[] = {
+   248, 247, 232, 231, 216, 215, 200, 199, 184, 183, 168, 167, 152, 151, 136, 135, 120, 119, 104, 103,  88,  87,  72,  71,  56,  55,  40,  39,  24,  23,   8,   7,
+   249, 246, 233, 230, 217, 214, 201, 198, 185, 182, 169, 166, 153, 150, 137, 134, 121, 118, 105, 102,  89,  86,  73,  70,  57,  54,  41,  38,  25,  22,   9,   6,
+   250, 245, 234, 229, 218, 213, 202, 197, 186, 181, 170, 165, 154, 149, 138, 133, 122, 117, 106, 101,  90,  85,  74,  69,  58,  53,  42,  37,  26,  21,  10,   5,
+   251, 244, 235, 228, 219, 212, 203, 196, 187, 180, 171, 164, 155, 148, 139, 132, 123, 116, 107, 100,  91,  84,  75,  68,  59,  52,  43,  36,  27,  20,  11,   4,
+   252, 243, 236, 227, 220, 211, 204, 195, 188, 179, 172, 163, 156, 147, 140, 131, 124, 115, 108,  99,  92,  83,  76,  67,  60,  51,  44,  35,  28,  19,  12,   3,
+   253, 242, 237, 226, 221, 210, 205, 194, 189, 178, 173, 162, 157, 146, 141, 130, 125, 114, 109,  98,  93,  82,  77,  66,  61,  50,  45,  34,  29,  18,  13,   2,
+   254, 241, 238, 225, 222, 209, 206, 193, 190, 177, 174, 161, 158, 145, 142, 129, 126, 113, 110,  97,  94,  81,  78,  65,  62,  49,  46,  33,  30,  17,  14,   1,
+   255, 240, 239, 224, 223, 208, 207, 192, 191, 176, 175, 160, 159, 144, 143, 128, 127, 112, 111,  96,  95,  80,  79,  64,  63,  48,  47,  32,  31,  16,  15,   0
+  };
+
+  uint8_t i = (y * LENGTH) + x;
+  uint8_t j = XYTable[i];
+  return j;
+}
+
 #endif
 
 
