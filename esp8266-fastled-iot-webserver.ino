5e85df7d621e35e82c88b08a35476043837ec551
Christoph Ketzler <christoph@ketzler.de>
2021-12-03 18:48:39 +0100

    removed juggle, it breaks other animations

--- esp8266-fastled-iot-webserver.ino
+++ esp8266-fastled-iot-webserver.ino
@@ -2100,42 +2100,42 @@ void bpm()
 #endif
 }
 
-void juggle()
-{
-    static uint8_t    numdots = 4; // Number of dots in use.
-    static uint8_t   faderate = 2; // How long should the trails be. Very low value = longer trails.
-    static uint8_t     hueinc = 255 / numdots - 1; // Incremental change in hue between each dot.
-    static uint8_t    thishue = 0; // Starting hue.
-    static uint8_t     curhue = 0; // The current hue
-    static uint8_t    thissat = 255; // Saturation of the colour.
-    static uint8_t thisbright = 255; // How bright should the LED/display be.
-    static uint8_t   basebeat = 5; // Higher = faster movement.
-
-    static uint8_t lastSecond = 99;  // Static variable, means it's only defined once. This is our 'debounce' variable.
-    uint8_t secondHand = (millis() / 1000) % 30; // IMPORTANT!!! Change '30' to a different value to change duration of the loop.
-
-    if (updatePatternBasedOnSpeedSetting(100) == false)
-        return;
-
-    if (lastSecond != secondHand) { // Debounce to make sure we're not repeating an assignment.
-        lastSecond = secondHand;
-        switch (secondHand) {
-        //case  0: numdots = 1; basebeat = 20; hueinc = 16; faderate = 2; thishue = 0; break; // You can change values here, one at a time , or altogether.
-        case 10: numdots = 4; basebeat = 10; hueinc = 16; faderate = 8; thishue = 128; break;
-        case 20: numdots = 8; basebeat = 5; hueinc = 0; faderate = 8; thishue = random8(); break; // Only gets called once, and not continuously for the next several seconds. Therefore, no rainbows.
-        case 30: break;
-        }
-    }
-
-    // Several colored dots, weaving in and out of sync with each other
-    curhue = thishue; // Reset the hue values.
-    fadeToBlackBy(leds, NUM_LEDS, faderate);
-    for (int i = 0; i < numdots; i++) {
-        //beat16 is a FastLED 3.1 function
-        leds[beatsin16(basebeat + i + numdots, 0, NUM_LEDS)] += CHSV(gHue + curhue, thissat, thisbright);
-        curhue += hueinc;
-    }
-}
+//void juggle()
+//{
+//    static uint8_t    numdots = 4; // Number of dots in use.
+//    static uint8_t   faderate = 2; // How long should the trails be. Very low value = longer trails.
+//    static uint8_t     hueinc = 255 / numdots - 1; // Incremental change in hue between each dot.
+//    static uint8_t    thishue = 0; // Starting hue.
+//    static uint8_t     curhue = 0; // The current hue
+//    static uint8_t    thissat = 255; // Saturation of the colour.
+//    static uint8_t thisbright = 255; // How bright should the LED/display be.
+//    static uint8_t   basebeat = 5; // Higher = faster movement.
+//
+//    static uint8_t lastSecond = 99;  // Static variable, means it's only defined once. This is our 'debounce' variable.
+//    uint8_t secondHand = (millis() / 1000) % 30; // IMPORTANT!!! Change '30' to a different value to change duration of the loop.
+//
+//    if (updatePatternBasedOnSpeedSetting(100) == false)
+//        return;
+//
+//    if (lastSecond != secondHand) { // Debounce to make sure we're not repeating an assignment.
+//        lastSecond = secondHand;
+//        switch (secondHand) {
+//        //case  0: numdots = 1; basebeat = 20; hueinc = 16; faderate = 2; thishue = 0; break; // You can change values here, one at a time , or altogether.
+//        case 10: numdots = 4; basebeat = 10; hueinc = 16; faderate = 8; thishue = 128; break;
+//        case 20: numdots = 8; basebeat = 5; hueinc = 0; faderate = 8; thishue = random8(); break; // Only gets called once, and not continuously for the next several seconds. Therefore, no rainbows.
+//        case 30: break;
+//        }
+//    }
+//
+//    // Several colored dots, weaving in and out of sync with each other
+//    curhue = thishue; // Reset the hue values.
+//    fadeToBlackBy(leds, NUM_LEDS, faderate);
+//    for (int i = 0; i < numdots; i++) {
+//        //beat16 is a FastLED 3.1 function
+//        leds[beatsin16(basebeat + i + numdots, 0, NUM_LEDS)] += CHSV(gHue + curhue, thissat, thisbright);
+//        curhue += hueinc;
+//    }
+//}
 
 void fire()
 {
